18dccd3c00d68b6a5e70143b9ae231634338b863
Yeongjin Lee <107300480+yeongjin-lee@users.noreply.github.com>
2026-01-14 19:53:24 +0900

    Update tokenization.py

--- src/common/tokenization.py
+++ src/common/tokenization.py
@@ -1,1 +1,6 @@
+# src/common/tokenization.py
+from transformers import AutoTokenizer
+
+def load_tokenizer(model_id: str):
+    return AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
 
